a229b9b818033e5dc241e6696e07b067f6eddc0d
tayyaba-ramzan <tayyabaramzan.pak@gmail.com>
2025-02-11 20:19:37 -0800

    Setting up the foundation for clean and scalable Python development.

---
+++ for-loop.py
@@ -0,0 +1,53 @@
+# For Loop in Python
+
+# range(5)
+# start=0
+# condition=<5
+# increment=by default 1
+# value=(0,1,2,3,4)
+
+# for n in range(5):
+#     print(n)
+
+# range(1,6)
+# start=1
+# condition=<6
+# increment=by default 1
+# value=(1,2,3,4,5)
+
+# for n in range(1,6):
+#     print(n)
+
+# range(1,8,2)
+# start=1
+# condition=<6
+# increment=2
+# value=(1,3,5,7)
+
+# for n in range(1,8,2):
+#     print(n)
+
+# Table of Two/(2):
+
+# for n in range(1,11,1):
+#     print(2,"X",n, "=",2*n)
+
+# 10 times print my Name:
+
+# for n in range(11):
+#     print("Tayyaba Ramzan")
+
+# Reverse:
+# range(10,0,-1)
+# start=10
+# condition=<0
+# derement=-1
+# value=(10,9,8,7,6,5,4,3,2,1)
+
+# for n in range(10,0,-1):
+#     print(n)
+
+
+for n in range(10,-1,-2):
+    print(n)
+    
---
+++ while-loop.py
@@ -0,0 +1,24 @@
+# While Loop in Python
+
+# start
+# condition
+# increment/decrement
+
+# While Loop Syntax:
+
+# i=1
+# while(i<=10):
+#     print(i,"Tayyaba Ramzan")
+#     i=i+1
+
+# Reverse:
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i=i-1
+
+i=10
+while(i>=1):
+    print(i)
+    i=i-2
