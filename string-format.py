6fcca7d84147ffc0b34b0eac09588a335ecb87c3
tayyaba-ramzan <tayyabaramzan.pak@gmail.com>
2025-02-17 01:01:56 -0800

    Setting up the foundation for clean and scalable Python development.

--- string-format.py
+++ string-format.py
@@ -8,3 +8,12 @@ print(txt2)
 
 txt3="Welcom to {} {}".format("Tayyaba","Ramzan")
 print(txt3)
+
+txt4="My value is {a:^10} {b}".format(a=10,b=30)
+print(txt4)
+
+txt5="My value is {a:<10} {b}".format(a=10,b=30)
+print(txt5)
+
+txt6="My value is {a:>10} {b}".format(a=10,b=30)
+print(txt6)
